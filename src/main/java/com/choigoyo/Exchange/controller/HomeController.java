78a27d9c00520a89290986bf2a022f8b681708ce
CHOIGOYO <choigoyo.q7@gmail.com>
2023-05-05 18:09:40 +0900

    메인페이지 css추가

--- src/main/java/com/choigoyo/Exchange/controller/HomeController.java
+++ src/main/java/com/choigoyo/Exchange/controller/HomeController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    //    기본페이지 반환
+    // 기본 페이지 반환
     @GetMapping("/")
     public String index(){
         return "main/index";
+    }
+
+    // 회원가입 페이지 반환
+    @GetMapping("/signInForm")
+    public String signInForm(){
+        return "signInForm";
+    }
+
+    // 로그인 페이지 반환
+    @GetMapping("loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 }
