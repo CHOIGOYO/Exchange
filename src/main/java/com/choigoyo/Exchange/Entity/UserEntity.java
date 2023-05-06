41b533c2aaccffbbeb24fb3dc96e8fdb9efa0db3
CHOIGOYO <choigoyo.q7@gmail.com>
2023-05-06 21:23:25 +0900

    회원가입 폼 다음주소 api 사용하기 수정

--- src/main/java/com/choigoyo/Exchange/Entity/UserEntity.java
+++ src/main/java/com/choigoyo/Exchange/Entity/UserEntity.java
@@ -24,8 +24,16 @@ public class UserEntity extends BaseTimeEntity{
     private String name; // 회원 이름
     @Column(name ="role",nullable = false)
     private String role = "ROLE_USER"; // 역할 디폴트 user
-    @Column(name ="address",nullable = true,length = 200)
-    private String address; // 회원 주소
+    @Column(name ="postcode",nullable = true,length = 20)
+    private Long postcode; // 회원 우편번호
+    @Column(name ="roadAddress",nullable = true,length = 200)
+    private String roadAddress; // 회원 도로명 주소
+    @Column(name ="jibunAddress",nullable = true,length = 200)
+    private String jibunAddress; // 회원 지번 주소
+    @Column(name ="detailAddress",nullable = true,length = 200)
+    private String detailAddress; // 회원 상세 주소
+    @Column(name ="extraAddress",nullable = true,length = 200)
+    private String extraAddress; // 회원 주소참고
     @Column(name ="phone",nullable = true,length = 20)
     private String phone; // 회원 전화번호
 
@@ -44,17 +52,20 @@ public class UserEntity extends BaseTimeEntity{
     @OneToMany(mappedBy = "user" , cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<HeartEntity> heart = new ArrayList<>(); // 하트
 
-
     @Builder
-    public UserEntity(Long id, String email, String password, String name, String role, String address, String phone,
-                      String provider, String providerId,
-                      List<BoardEntity> board, List<CommentEntity> comment, List<HeartEntity> heart) {
+    public UserEntity(Long id, String email, String password, String name, String role,
+                      Long postcode, String roadAddress, String jibunAddress, String detailAddress, String extraAddress,
+                      String phone, String provider, String providerId, List<BoardEntity> board, List<CommentEntity> comment, List<HeartEntity> heart) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.name = name;
         this.role = role;
-        this.address = address;
+        this.postcode = postcode;
+        this.roadAddress = roadAddress;
+        this.jibunAddress = jibunAddress;
+        this.detailAddress = detailAddress;
+        this.extraAddress = extraAddress;
         this.phone = phone;
         this.provider = provider;
         this.providerId = providerId;
@@ -62,5 +73,10 @@ public class UserEntity extends BaseTimeEntity{
         this.comment = comment;
         this.heart = heart;
     }
+
+
+
+
+
 
 }
